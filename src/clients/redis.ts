d049f28aefdbc397e6c692a675568609424e8869
Aman Rai <71575102+Cypher-aman@users.noreply.github.com>
2024-03-10 19:58:58 +0530

    Update redis.ts

--- src/clients/redis.ts
+++ src/clients/redis.ts
@@ -1,7 +1,6 @@
 import Redis from 'ioredis';
 
 const redisClient = new Redis(
-  `rediss://default:${process.env.REDIS_CLIENT_PASS}@${process.env.REDIS_HOST}`
+  `redis://default:${process.env.REDIS_CLIENT_PASS}@${process.env.REDIS_HOST}:44727`
 );
-
 export default redisClient;
