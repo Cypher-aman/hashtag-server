aa9a1d65a251d85a80a69f6249d0fd40f37e5abf
Cypher-aman <raiaman987@gmail.com>
2024-03-16 06:05:21 +0530

    added '/' route

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,7 +10,8 @@ import { verifyJWTUserToken } from '../utils/helper';
 const initServer = async () => {
   const app = express();
   app.use(cors());
-  app.use('/health', (req, res) => res.status(200).send('OK'));
+  app.use('/health', (req, res) => res.status(200).send('HEALTHY'));
+  app.use('/', (req, res) => res.status(200).send('OK'));
 
   const server = new ApolloServer<GraphQlContext>({
     typeDefs: `
